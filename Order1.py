a41275f1e06b84fe6355478b59ac434303871ca6
pengshuai@eastmoney.com <acheng6@126.com>
2026-06-23 20:22:38 +0800

    优化单号不匹配

--- Order1.py
+++ Order1.py
@@ -196,9 +196,11 @@ def get_product_id(product, color_id, color):
     }
     info = requests.post(url, headers=headers, data=json.dumps(params)).json()
     result = list()
+    new_product = []
     for i in info['resp'][1]:  # 多个结果
         # if i.get('code') and i.get('code') != str(product):
         #     continue
+        new_product.append(i.get('code'))
         styles = i['styleText']
         if isinstance(styles, str):
             if '/' in styles:
@@ -217,7 +219,8 @@ def get_product_id(product, color_id, color):
                 else:
                     if f'{color_id}{color}' in re.sub(' ', '', style):
                         result.append(i['productCode'])
-    return result
+    new_product = list(set(new_product))[0]
+    return new_product, result
 
 def get_product_size_code(product, product_id, color_id, size):
     """根据product_id、color_id、size获取product_size_code
@@ -534,7 +537,8 @@ def get_count(order_id, order_info):
     if order_info["color_id"] == 'null':
         logger.info(f'订单:{order_id}，{order_info["title"]}无颜色编号')
         return order_info
-    product_id = get_product_id(order_info["product"], order_info["color_id"], order_info["color"])
+    new_product, product_id = get_product_id(order_info["product"], order_info["color_id"], order_info["color"])
+    order_info["product"]= new_product
     if not product_id:
         order_info['title'] += '优衣库商品已下架'
         return order_info
@@ -548,6 +552,7 @@ def get_count(order_id, order_info):
         order_info['title'] += '优衣库无对应尺寸'
         return order_info
     count = get_product_count(product_id, product_size_code)
+    print(count)
     if count == 0:
         order_info['title'] += '优衣库商品已售罄'
         order_info['flag'] = 'error'
